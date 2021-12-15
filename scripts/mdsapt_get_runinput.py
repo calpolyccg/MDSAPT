264930fcf0b08d6ff8ba69de9bb0bb5adfc60a6f
ALescoulie <alescoul@calpoly.edu>
2021-12-15 10:30:12 -0800

    fix typing mdsapt_get_runinput.py

--- scripts/mdsapt_get_runinput.py
+++ scripts/mdsapt_get_runinput.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
-import logging
-logger = logging.getLogger('mdsapt')
-
 from argparse import ArgumentParser
 
+import logging
+
+logger = logging.getLogger('mdsapt')
 
 if __name__ == '__main__':
     parser = ArgumentParser(usage=__doc__)
-    parser.add_argument('filename', metavar='F', type='str', nargs='?',
+    parser.add_argument('filename', metavar='F', type=str, nargs='?',
                         default='input.yaml',
                         help='Filename for generated input file input.yaml')
 
     filename = vars(parser.parse_args())['filename']
 
     with open('./mdsapt/data/template_input.yaml', 'r') as template:
-        template_data = template
+        template_data = template.read()
 
     with open(f'{filename}.yaml', 'w+') as new_file:
         new_file.write(template_data)
