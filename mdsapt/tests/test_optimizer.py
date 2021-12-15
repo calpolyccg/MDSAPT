e9e500c10867742c3a93a1f6bcbbeb88a3f665dc
ALescoulie <alescoul@calpoly.edu>
2021-12-15 00:35:03 -0800

    add test_optimizer.py, update test_input.yaml

---
+++ mdsapt/tests/test_optimizer.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+
+
+import os
+
+import MDAnalysis as mda
+
+from ..optimizer import Optimizer
+from ..reader import InputReader
+
+
+class TestOptimizer(object):
+
+    def test_prepare_resids(self):
+        settings = InputReader(os.path.join(os.getcwd(), 'mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
+        opt: Optimizer = Optimizer(settings)
