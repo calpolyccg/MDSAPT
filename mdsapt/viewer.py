2020464ad45a23fe9ac46323fdd3352a09f3a586
ALescoulie <alescoul@calpoly.edu>
2021-12-29 12:40:22 -0800

    update viewer.py types

--- mdsapt/viewer.py
+++ mdsapt/viewer.py
@@ -40,13 +40,12 @@ class Viewer(object):
 
     @staticmethod
     def _launch_viewer(system: Union[mda.Universe, mda.AtomGroup], **nglview_kwargs) -> None:
-        nv.show_mdanalysis(system, **nglview_kwargs)
+        return nv.show_mdanalysis(system, **nglview_kwargs)
 
-    def view_system(self, **nglview_kwargs) -> None:
-        """Shows entire trajectory"""
+    def view_system(self, **nglview_kwargs) -> nv.NGLWidget:
         self._launch_viewer(self._unv, **nglview_kwargs)
 
-    def view_residue(self, resid: int, **nglview_kwargs) -> None:
+    def view_residue(self, resid: int, **nglview_kwargs) -> nv.NGLWidget:
         """Shows selected residue.
 
         :Arguments:
@@ -55,9 +54,9 @@ class Viewer(object):
             *nglview_kwargs*
                 arguments passed to the viewer"""
         resid: mda.AtomGroup = self._unv.select_atoms(f'resid {resid}')
-        self._launch_viewer(resid, **nglview_kwargs)
+        return self._launch_viewer(resid, **nglview_kwargs)
 
-    def view_interaction_pair(self, resid1: int, resid2: int, **nglview_kwargs) -> None:
+    def view_interaction_pair(self, resid1: int, resid2: int, **nglview_kwargs) -> nv.NGLWidget:
         """Shows selected pair of residues.
 
         :Arguments:
@@ -70,9 +69,9 @@ class Viewer(object):
         r1: mda.AtomGroup = self._unv.select_atoms(f'resid {resid1}')
         r2: mda.AtomGroup = self._unv.select_atoms(f'resid {resid2}')
         r_pair: mda.AtomGroup = r1 + r2
-        self._launch_viewer(r_pair, **nglview_kwargs)
+        return self._launch_viewer(r_pair, **nglview_kwargs)
 
-    def view_optimized_residue(self, resid: int, **nglview_kwargs) -> None:
+    def view_optimized_residue(self, resid: int, **nglview_kwargs) -> nv.NGLWidget:
         """Shows selected residue after prepared for SAPT by
         :class:`mdsapt.optimizer.
 
@@ -82,9 +81,9 @@ class Viewer(object):
             *nglview_kwargs*
                 arguments passed to the viewer"""
         resid = self._opt.rebuild_resid(resid, self._unv.select_atoms(f'resid {resid}'))
-        self._launch_viewer(resid, **nglview_kwargs)
+        return self._launch_viewer(resid, **nglview_kwargs)
 
-    def view_optimized_interaction_pair(self, resid1: int, resid2: int, **nglview_kwargs) -> None:
+    def view_optimized_interaction_pair(self, resid1: int, resid2: int, **nglview_kwargs) -> nv.NGLWidget:
         """Shows selected pair of residues after prepared for SAPT by
         :class:`mdsapt.optimizer.
 
@@ -99,4 +98,4 @@ class Viewer(object):
         r_pair.add_TopologyAttr('masses', [x for x in r1.mases] + [x for x in r2.masses])
         r_pair.add_TopologyAttr('name', [x for x in r1.names] + [x for x in r2.names])
         r_pair.atoms.positions = np.row_stack((r1.positions + r2.positions))
-        self._launch_viewer(r_pair, **nglview_kwargs)
+        return self._launch_viewer(r_pair, **nglview_kwargs)
