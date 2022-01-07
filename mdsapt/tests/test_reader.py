e815660a7612666a723132d880a633840fa374f5
ALescoulie <alescoul@calpoly.edu>
2022-01-07 14:06:54 -0800

    increase coverage

--- mdsapt/tests/test_reader.py
+++ mdsapt/tests/test_reader.py
@@ -1,3 +1,5 @@
+import copy
+
 import pytest
 
 import os
@@ -8,7 +10,7 @@ from ..reader import InputReader, InputError
 class TestInputReader(object):
     reference = {
         'topology_path': 'mdsapt/tests/testing_resources/testtop.psf',
-        'trajectory_path': ['mdsapt/tests/testing_resources/testtraj.dcd'],
+        'trajectory_paths': ['mdsapt/tests/testing_resources/testtraj.dcd'],
         'selection_resid_num': [11, 199],
         'int_pairs': [[11, 199]],
         'trajectory_settings': {
@@ -20,24 +22,57 @@ class TestInputReader(object):
             'ncpus': 4,
             'memory': '12GB',
             'time': '24:00:00'
+        },
+        'opt_settings': {
+            'pH': 7
+        },
+        'sapt_settings': {
+            'method': 'sapt0',
+            'basis': 'jun-cc-pvdz',
+            'settings': {'reference': 'rhf'},
+            'save_psi4_output': True
         }
     }
 
     def test_reader(self):
         settings = InputReader(os.path.join(os.getcwd(), 'mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
         assert settings.top_path == self.reference['topology_path']
-        assert settings.trj_path == self.reference['trajectory_path']
+        assert settings.trj_path == self.reference['trajectory_paths']
         assert settings.ag_sel == self.reference['selection_resid_num']
         assert settings.ag_pair == self.reference['int_pairs']
         assert settings.trj_settings == self.reference['trajectory_settings']
         assert settings.sys_settings == self.reference['system_settings']
 
-    @pytest.mark.parametrize('key_pair', [['topology_path', 'error'], ['trajectory_path', ['error']],
-                                          ['selection_resid_num', [0]], ['int_pairs', [1]],
-                                          ['trajectory_settings', []], ['system_settings', []]])
+    @pytest.mark.parametrize('key_pair', [['topology_path', 'error'], ['trajectory_paths', ['error']],
+                                          ['selection_resid_num', [0]], ['int_pairs', [[1]]],
+                                          ['int_pairs', [[1, 199]]], ['int_pairs', [[11, 1]]]])
     def test_check_runinputs(self, key_pair):
-        settings = InputReader(os.path.join('mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
-        alt_set = self.reference
+        alt_set = self.reference.copy()
         alt_set[key_pair[0]] = key_pair[1]
         with pytest.raises(InputError):
-            settings._check_inputs(alt_set)
+            InputReader._check_inputs(alt_set)
+
+    def test_fail_load_input(self):
+        with pytest.raises(InputError):
+            settings = InputReader(os.path.join('mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
+            settings.load_input('error')
+
+    def test_missing_yaml_data0(self):
+        alt_set = copy.deepcopy(self.reference)
+        alt_set.pop('topology_path')
+        with pytest.raises(InputError):
+            InputReader._check_inputs(alt_set)
+
+    def test_missing_yaml_data1(self):
+        alt_set = copy.deepcopy(self.reference)
+        alt_set['opt_settings'].pop('pH')
+        with pytest.raises(InputError):
+            InputReader._check_inputs(alt_set)
+
+    @pytest.mark.parametrize('key_pair', [['start', 10], ['step', 10],
+                                          ['step', 0], ['stop', 1000]])
+    def test_error_in_trj_settings(self, key_pair):
+        alt_set = copy.deepcopy(self.reference)
+        alt_set['trajectory_settings'][key_pair[0]] = key_pair[1]
+        with pytest.raises(InputError):
+            InputReader._check_inputs(alt_set)
