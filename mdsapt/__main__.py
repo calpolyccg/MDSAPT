dd8705d9d3b909c50201ab033ab6657ccb28275c
Astrid Yu <astrid@astrid.tech>
2022-03-20 22:38:09 -0700

    Fix and optimize CLI

--- mdsapt/__main__.py
+++ mdsapt/__main__.py
@@ -1,4 +1,10 @@
-from . import cli
+"""
+Main entrypoint for the CLI.
+"""
+
+# Note that we import directly from the CLI instead of the full package. This is an optimization
+# since some CLI tasks do not need the entire package.
+from .cli import cli
 
 
 if __name__ == '__main__':
