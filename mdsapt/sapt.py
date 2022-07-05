fd80109c596e75045ba85d73ab6b608316c470f4
alescoulie <alia.lescoulie@gmail.com>
2022-07-05 12:10:30 -0700

    refactor TrajectorySAPT and SAPT to accept Config objects

--- mdsapt/sapt.py
+++ mdsapt/sapt.py
@@ -16,7 +16,7 @@ calculations between the residues selected in the input file.
 """
 
 from tkinter.ttk import Progressbar
-from typing import Dict, List
+from typing import Dict, List, Set, Tuple
 
 import pandas as pd
 
@@ -28,10 +28,11 @@ from MDAnalysis.converters.RDKit import atomgroup_to_mol
 from MDAnalysis.lib.log import ProgressBar
 
 import psi4
+from pydantic import ValidationError
 
 from rdkit import Chem
 
-from .reader import InputReader
+from .config import Config, TrajectoryAnalysisConfig
 from .optimizer import Optimizer, get_spin_multiplicity
 from .utils.ensemble import Ensemble, EnsembleAtomGroup
 
@@ -39,29 +40,31 @@ import logging
 
 logger = logging.getLogger('mdsapt.sapt')
 
+
 class SAPT(object):
     """Contains methods for running SAPT calculations on molecular dynamics data. Used as the super class for other SAPT tools in the library."""
 
     _opt: Optimizer
-    _cfg: InputReader
-    _mem: int
+    _cfg: Config
+    _mem: str
     _save_psi_out: bool
     _method: str
     _basis: str
     _settings: Dict[str, str]
-    _mht_to_kcalmol: float = 627.509 
-    
-    def __init__(self, config: InputReader, optimizer: Optimizer) -> None:
+    _mht_to_kcalmol: float = 627.509
+
+    def __init__(self, config: Config, optimizer: Optimizer) -> None:
         self._opt = optimizer
         self._cfg = config
-        self._mem = config.memory
-        self._save_psi_out = config.sapt_out
-        self._method = config.sapt_method
-        self._basis = config.sapt_basis
-        self._settings = config.sapt_settings['settings']
+        self._mem = config.system_limits.memory
+        self._save_psi_out = config.psi4.save_output
+        self._method = config.psi4.method
+        self._basis = config.psi4.basis
+        self._settings = config.psi4.settings
 
     def get_psi_mol(self, key, resid: mda.AtomGroup) -> str:
-        """Generates Psi4 input file the specified residue. Prepares amino acids for SAPT using :class:`mdsapt.optimizer.Optimizer`. Adds charge and spin multiplicity to top of cooridnates."""
+        """Generates Psi4 input file the specified residue. Prepares amino acids for SAPT using
+        :class:`mdsapt.optimizer.Optimizer`. Adds charge and spin multiplicity to top of cooridnates. """
         resid: mda.AtomGroup = self._opt.rebuild_resid(key, resid)
         rd_mol = atomgroup_to_mol(resid)
 
@@ -69,9 +72,10 @@ class SAPT(object):
         for atom in resid.atoms:
             coords += f'\n{atom.element} {atom.position[0]} {atom.position[1]} {atom.position[2]}'
         return coords
-    
+
     def calc_SAPT(self, input: str, filename: str) -> Dict[str, float]:
-            """Runs SAPT on the molecules given in the input string. If `save_psi4_output` is set to true the output will be saved as the given filename.
+        """Runs SAPT on the molecules given in the input string. If `save_psi4_output` is set to true the output will
+        be saved as the given filename.
             
             Results are returned in a dictionary with the SAPT energy broken down by type with the following keys.
             
@@ -80,33 +84,33 @@ class SAPT(object):
             3. SAPT EXCH ENERGY
             4. SAPT IND ENERGY
             5. SAPT DISP ENERGY"""
-            dimer = psi4.geometry(input)
-            psi4.set_options(self._settings)
-            psi4.set_memory(self._mem)
-            psi4.set_num_threads(self._cfg.ncpus)
+        dimer = psi4.geometry(input)
+        psi4.set_options(self._settings)
+        psi4.set_memory(self._mem)
+        psi4.set_num_threads(self._cfg.system_limits.ncpus)
 
-            if self._save_psi_out:
-                psi4.set_output_file(filename)  # Saves output file
+        if self._save_psi_out:
+            psi4.set_output_file(filename)  # Saves output file
 
-            # Calculating SAPT
-            psi4.energy(f'{self._method}/{self._basis}', molecule=dimer)
+        # Calculating SAPT
+        psi4.energy(f'{self._method}/{self._basis}', molecule=dimer)
 
-            result: Dict[str, float] = {
-                'SAPT TOTAL ENERGY': 0.0,
-                'SAPT ELST ENERGY': 0.0,
-                'SAPT EXCH ENERGY': 0.0,
-                'SAPT IND ENERGY': 0.0,
-                'SAPT DISP ENERGY': 0.0
-            }
+        result: Dict[str, float] = {
+            'SAPT TOTAL ENERGY': 0.0,
+            'SAPT ELST ENERGY': 0.0,
+            'SAPT EXCH ENERGY': 0.0,
+            'SAPT IND ENERGY': 0.0,
+            'SAPT DISP ENERGY': 0.0
+        }
 
-            # Getting results
-            result['SAPT TOTAL ENERGY'] = psi4.variable('SAPT TOTAL ENERGY')*self._mht_to_kcalmol
-            result['SAPT ELST ENERGY'] = psi4.variable('SAPT ELST ENERGY')*self._mht_to_kcalmol
-            result['SAPT EXCH ENERGY'] = psi4.variable('SAPT EXCH ENERGY')*self._mht_to_kcalmol
-            result['SAPT IND ENERGY'] = psi4.variable('SAPT IND ENERGY')*self._mht_to_kcalmol
-            result['SAPT DISP ENERGY'] = psi4.variable('SAPT DISP ENERGY')*self._mht_to_kcalmol
+        # Getting results
+        result['SAPT TOTAL ENERGY'] = psi4.variable('SAPT TOTAL ENERGY') * self._mht_to_kcalmol
+        result['SAPT ELST ENERGY'] = psi4.variable('SAPT ELST ENERGY') * self._mht_to_kcalmol
+        result['SAPT EXCH ENERGY'] = psi4.variable('SAPT EXCH ENERGY') * self._mht_to_kcalmol
+        result['SAPT IND ENERGY'] = psi4.variable('SAPT IND ENERGY') * self._mht_to_kcalmol
+        result['SAPT DISP ENERGY'] = psi4.variable('SAPT DISP ENERGY') * self._mht_to_kcalmol
 
-            return result            
+        return result
 
 
 class TrajectorySAPT(SAPT, AnalysisBase):
@@ -120,10 +124,10 @@ class TrajectorySAPT(SAPT, AnalysisBase):
 
     _unv: mda.Universe
     _sel: Dict[int, mda.AtomGroup]
-    _sel_pairs: List[List[int]]
+    _sel_pairs: List[Tuple[int, int]]
     results: pd.DataFrame
 
-    def __init__(self, config: InputReader, optimizer: Optimizer, **universe_kwargs) -> None:
+    def __init__(self, config: Config, optimizer: Optimizer, **universe_kwargs) -> None:
         """Sets up Trajectory and residue selections.
 
         :Arguments:
@@ -135,11 +139,20 @@ class TrajectorySAPT(SAPT, AnalysisBase):
             *universe_arguments*
                 keyword arguments for loading the trajectory into a MDAnalysis :class:`Universe <MDAnalysis.core.groups.universe.Universe>`
         """
-        self._unv = mda.Universe(config.top_path, config.trj_path, **universe_kwargs)
+        try:
+            # Ensuring config type is correct
+            if not isinstance(config.analysis, TrajectoryAnalysisConfig):
+                raise ValidationError("config.analysis.type is not trajectory")
+        except ValidationError as err:
+            logger.exception(err)
+            raise err
+        self._unv = config.analysis.get_universe(**universe_kwargs)
         elements = guess_types(self._unv.atoms.names)
         self._unv.add_TopologyAttr('elements', elements)
-        self._sel = {x: self._unv.select_atoms(f'resid {x} and not (name OH2 or name H1 or name H2)') for x in config.ag_sel}
-        self._sel_pairs = config.ag_pair
+        ag_sel: Set[int] = config.analysis.get_selections()
+        self._sel = {x: self._unv.select_atoms(f'resid {x} and not (name OH2 or name H1 or name H2)')
+                     for x in ag_sel}
+        self._sel_pairs = config.analysis.pairs
         SAPT.__init__(self, config, optimizer)
         AnalysisBase.__init__(self, self._unv.trajectory)
 
@@ -149,17 +162,18 @@ class TrajectorySAPT(SAPT, AnalysisBase):
         self.results = pd.DataFrame(columns=self._col)
         self._res_dict = {x: [] for x in self._col}
 
-
     def _single_frame(self) -> None:
         xyz_dict = {k: self.get_psi_mol(k, self._sel[k]) for k in self._sel.keys()}
         for pair in self._sel_pairs:
             coords = xyz_dict[pair[0]] + '\n--\n' + xyz_dict[pair[1]] + '\nunits angstrom'
-            
+
             logger.info(f'Starting SAPT for {pair}')
 
             sapt: Dict[str, float] = self.calc_SAPT(coords, f'sapt_{pair[0]}-{pair[1]}_{self._ts.time}.out')
-            result = [f'{pair[0]}-{pair[1]}', self._ts.time] + [sapt[x] for x in ['SAPT TOTAL ENERGY', 'SAPT ELST ENERGY',
-            'SAPT EXCH ENERGY','SAPT IND ENERGY','SAPT DISP ENERGY']]
+            result = [f'{pair[0]}-{pair[1]}', self._ts.time] + [sapt[x] for x in
+                                                                ['SAPT TOTAL ENERGY', 'SAPT ELST ENERGY',
+                                                                 'SAPT EXCH ENERGY', 'SAPT IND ENERGY',
+                                                                 'SAPT DISP ENERGY']]
 
             for r in range(len(result)):
                 self._res_dict[self._col[r]].append(result[r])
@@ -171,13 +185,14 @@ class TrajectorySAPT(SAPT, AnalysisBase):
 
 class DockingSAPT(SAPT):
     """"""
-    
+
     _ens: Ensemble
     _sel: Dict[int, EnsembleAtomGroup]
 
-    def __init__(self, config: InputReader, optimizer: Optimizer) -> None:
-        self._ens = Ensemble(config.path)
-        self._sel = {k: self._ens.select_atoms(f'resid {k} and not (name OH2 or name H1 or name H2') for k in self._cfg.ag_sel}
+    def __init__(self, config: Config, optimizer: Optimizer) -> None:
+        self._ens = Ensemble(config.)
+        self._sel = {k: self._ens.select_atoms(f'resid {k} and not (name OH2 or name H1 or name H2') for k in
+                     self._cfg.ag_sel}
         super(DockingSAPT, self).__init__(config, optimizer)
 
     def _prepare(self) -> None:
@@ -190,12 +205,13 @@ class DockingSAPT(SAPT):
         xyz_dict = {k: self.get_psi_mol(self._sel[k][self._key]) for k in self._sel.keys()}
         for pair in self._sel_pairs:
             coords = xyz_dict[pair[0]] + '\n--\n' + xyz_dict[pair[1]] + '\nunits angstrom'
-            
+
             logger.info(f'Starting SAPT for {pair}')
 
             sapt: Dict[str, float] = self.calc_SAPT(coords, f'sapt_{pair[0]}-{pair[1]}_{self._key}.out')
             result = [f'{pair[0]}-{pair[1]}', self._key] + [sapt[x] for x in ['SAPT TOTAL ENERGY', 'SAPT ELST ENERGY',
-            'SAPT EXCH ENERGY','SAPT IND ENERGY','SAPT DISP ENERGY']]
+                                                                              'SAPT EXCH ENERGY', 'SAPT IND ENERGY',
+                                                                              'SAPT DISP ENERGY']]
 
             for r in range(len(result)):
                 self._res_dict[self._col[r]].append(result[r])
