fc470c1401f63d2c7cdbc6f5472ff3a72ea16602
alescoulie <alia.lescoulie@gmail.com>
2022-07-14 15:05:46 -0700

    fix SAPT config loading

--- mdsapt/tests/test_sapt.py
+++ mdsapt/tests/test_sapt.py
@@ -7,22 +7,19 @@ from MDAnalysis.topology.guessers import guess_types
 
 from ..config import Config, load_from_yaml_file
 from ..sapt import TrajectorySAPT
-from ..optimizer import Optimizer
 
 
-class TestSAPT(object):
+class TestSAPT:
 
     def setup(self):
         self.settings: Config = load_from_yaml_file(
             os.path.join(os.getcwd(), 'mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
-        self.opt = Optimizer(self.settings)
-        self.Unv = mda.Universe(str(self.settings.analysis.topology),
-                                [str(path) for path in self.settings.analysis.trajectories])
-        elements = guess_types(self.Unv.atoms.names)
-        self.Unv.add_TopologyAttr('elements', elements)
+        self.unv = self.settings.analysis.create_universe()
+        elements = guess_types(self.unv.atoms.names)
+        self.unv.add_TopologyAttr('elements', elements)
 
     def test_run_sapt(self):
-        SAPT12 = TrajectorySAPT(self.settings, self.opt)
+        SAPT12 = TrajectorySAPT(self.settings)
         SAPT12.run(1, 2)
         cols_act = SAPT12.results.columns
         cols_exp = ['residues', 'time', 'total', 'electrostatic', 'exchange', 'induction', 'dispersion']
