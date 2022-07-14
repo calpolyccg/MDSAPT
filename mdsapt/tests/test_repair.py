34e93f8fa3b046a8f958dac6be50282359ff68ee
alescoulie <alia.lescoulie@gmail.com>
2022-07-14 15:04:10 -0700

    update tests for repair

--- mdsapt/tests/test_repair.py
+++ mdsapt/tests/test_repair.py
@@ -1,26 +1,20 @@
-import numpy as np
+from pathlib import Path
+
 from numpy.testing import assert_array_almost_equal
-import pytest
-
-
-import os
 
 import MDAnalysis as mda
 
 from ..repair import rebuild_resid
-from ..config import Config, load_from_yaml_file
+
+resources_dir = Path(__file__).parent / 'testing_resources'
+
+unv: mda.Universe = mda.Universe(f'{resources_dir}/testtop.psf', f'{resources_dir}/testtraj.dcd')
 
 
 class TestOptimizer(object):
 
     def test_prepare_resids(self):
-        settings: Config = load_from_yaml_file(
-            os.path.join(os.getcwd(), 'mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
-        settings.analysis.pairs = [(11, 214)]
-        Unv: mda.Universe = settings.analysis.create_universe()
-
-        r11: mda.AtomGroup = Unv.select_atoms('resid 11')
-
+        r11: mda.AtomGroup = unv.select_atoms('resid 11')
         r11_fixed: mda.AtomGroup = rebuild_resid(11, r11)
         assert_array_almost_equal(r11.select_atoms(f'name CA').positions, r11_fixed.select_atoms(f'name CA').positions, decimal=3)
         assert_array_almost_equal(r11.select_atoms(f'name N').positions, r11_fixed.select_atoms(f'name N').positions, decimal=3)
@@ -28,21 +22,11 @@ class TestOptimizer(object):
         assert_array_almost_equal(r11.select_atoms(f'name C').positions, r11_fixed.select_atoms(f'name C').positions, decimal=3)
 
     def test_prepare_end(self):
-        settings: Config = load_from_yaml_file(
-            os.path.join(os.getcwd(), 'mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
-        Unv: mda.Universe = settings.analysis.create_universe()
-        settings.analysis.pairs = [(1, 214)]
-
-        r215 = Unv.select_atoms('resid 214')
+        r215 = unv.select_atoms('resid 214')
         r215_fixed = rebuild_resid(214, r215)
         assert len(r215_fixed.select_atoms('name Hc')) == 0
 
     def test_non_amino(self):
-        settings: Config = load_from_yaml_file(
-            os.path.join(os.getcwd(), 'mdsapt', 'tests', 'testing_resources', 'test_input.yaml'))
-        settings.analysis.pairs = [(126, 214)]
-        Unv: mda.Universe = settings.analysis.create_universe()
-
-        r126 = Unv.select_atoms('resid 126')
+        r126 = unv.select_atoms('resid 126')
         r126_fixed = rebuild_resid(126, r126)
         assert len(r126_fixed.select_atoms('name Hc')) == 0
