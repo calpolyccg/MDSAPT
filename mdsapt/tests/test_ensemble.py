5fd6d522576c236464d00d9b78257a531206bbe0
alescoulie <alia.lescoulie@gmail.com>
2022-07-14 15:06:03 -0700

    add ensemble tests

--- mdsapt/tests/test_ensemble.py
+++ mdsapt/tests/test_ensemble.py
@@ -4,7 +4,7 @@ from typing import List, Literal
 import pytest
 from pydantic import FilePath
 
-from ..config import DockingStructureMode, DockingAnalysisConfig
+from ..config import DockingAnalysisConfig
 from ..utils.ensemble import Ensemble
 
 resources_dir = Path(__file__).parent / 'testing_resources'
@@ -12,24 +12,24 @@ resources_dir = Path(__file__).parent / 'testing_resources'
 
 working_docking_settings = dict(
     type='docking',
-    mode=DockingStructureMode.SeparateLigand,
     protein=f'{resources_dir}/docking_sep_test/2hnt.pdb',
     ligands=[f'{resources_dir}/docking_sep_test/ligands/15U0.pdb',
              f'{resources_dir}/docking_sep_test/ligands/15U1.pdb',
              f'{resources_dir}/docking_sep_test/ligands/98P_2.pdb'],
-    combined_topologies=[f'{resources_dir}/docking_merged_test/2hnt_15U0.pdb',
-                         f'{resources_dir}/docking_merged_test/2hnt_98P.pdb'],
     pairs=[(3, 14)]
 )
-
-docking_config: DockingAnalysisConfig = DockingAnalysisConfig(**working_docking_settings)
 
 
 def test_list_of_merged() -> None:
     merged_list = [f'{resources_dir}/docking_merged_test/2hnt_15U0.pdb',
                    f'{resources_dir}/docking_merged_test/2hnt_98P.pdb']
 
-    ens: Ensemble = Ensemble(DockingStructureMode.MergedLigand,
-                             systems_dir=docking_config.combined_topologies)
+    ens: Ensemble = Ensemble.build_from_files(merged_list)
+    assert all(k in merged_list for k in ens.keys())
 
+
+def test_dir_of_merged() -> None:
+    ens: Ensemble = Ensemble.build_from_dir(f'{resources_dir}/docking_merged_test')
+
+    assert len(ens) == 2
 
