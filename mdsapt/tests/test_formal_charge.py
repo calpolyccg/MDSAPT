703e312f87f0657ea349b01c2b5c76d19665fada
Astrid Yu <astrid@astrid.tech>
2022-08-04 22:29:55 +0000

    Further testing of formal charge

--- mdsapt/tests/test_formal_charge.py
+++ mdsapt/tests/test_formal_charge.py
@@ -1,16 +1,44 @@
+from typing import Optional
 import pytest
-from ..utils.formal_charge import get_fc
+from ..utils.formal_charge import _get_fc, calculate_electron_info, ElectronInfo
 
-@pytest.mark.parametrize('element, bonds, expected', [
-    ('C', 2, 0),
-    ('C', 3, -1),
-    ('C', 4, 0),
-    ('O', 2, 0),
-    ('O', 3, 1),
-    ('O', 1, -1),
-    ('N', 2, -1),
-    ('N', 3, 0),
-    ('N', 4, 1),
+
+@pytest.mark.parametrize('element, bonds, fc, fields', [
+    # fmt: off
+
+    ('H',  1, None, dict(valence=1, fc= 0, lone=0, radical=0)),
+
+    # Typical C bonding
+    ('C',  2, None, dict(valence=4, fc= 0, lone=2, radical=0)),  # Carbene
+    ('C',  3, None, dict(valence=4, fc=-1, lone=2, radical=0)),  # C in Carbon monoxide (CO)
+    ('C',  4, None, dict(valence=4, fc= 0, lone=0, radical=0)),  # Normal C
+    ('C',  1,    0, dict(valence=4, fc= 0, lone=3, radical=3)),  # Carbyne
+    ('C',  3,   -1, dict(valence=4, fc=-1, lone=2, radical=0)),  # Carboanion
+    ('C',  3,    1, dict(valence=4, fc= 1, lone=0, radical=0)),  # Carbocation
+
+    ('N',  1, None, dict(valence=5, fc= 0, lone=4, radical=2)),  # Nitrene
+    ('N',  2, None, dict(valence=5, fc=-1, lone=4, radical=0)),  # N in Nitric oxide
+    ('N',  3, None, dict(valence=5, fc= 0, lone=2, radical=0)),  # Normal N
+    ('N',  4, None, dict(valence=5, fc= 1, lone=0, radical=0)),  # N in NH4+
+
+    ('O',  1, None, dict(valence=6, fc=-1, lone=6, radical=0)),  # O in Hydroxide ion (OH-)
+    ('O',  2, None, dict(valence=6, fc= 0, lone=4, radical=0)),  # Normal O
+    ('O',  3, None, dict(valence=6, fc= 1, lone=2, radical=0)),  # O in Hydronium (H3O+)
+    ('O',  1,    0, dict(valence=6, fc= 0, lone=5, radical=1)),  # O in Hydroxyl radical (OH)
+
+    ('P',  5, None, dict(valence=5, fc= 0, lone=0, radical=0)),  # P in Phosphate
+    ('P',  6, None, dict(valence=5, fc=-1, lone=0, radical=0)),  # P in PF6-
+
+    # Halogens
+    ('F',  1, None, dict(valence=7, fc= 0, lone=6, radical=0)),
+    ('Cl', 1, None, dict(valence=7, fc= 0, lone=6, radical=0)),
+    ('Br', 1, None, dict(valence=7, fc= 0, lone=6, radical=0)),
+    ('I',  1, None, dict(valence=7, fc= 0, lone=6, radical=0)),
+
+    # fmt: on
 ])
-def test_formal_charge_is_accurate(element: str, bonds: int, expected: int):
-    assert get_fc(element, bonds) == expected
+def test_calculate_electron_info(element: str, bonds: int, fc: Optional[int], fields: dict):
+    actual = calculate_electron_info(element, bonds, fc)
+
+    expected = ElectronInfo(element=element, bonds=bonds, **fields)
+    assert actual == expected
--- mdsapt/utils/formal_charge.py
+++ mdsapt/utils/formal_charge.py
@@ -1,35 +1,99 @@
-ELEMENT_TO_VALENCE = {
+r"""
+:mod:`mdsapt.utils.formal_charge` -- Utilities for calculating charge-related properties of an atom
+===================================================================================================
+
+.. autofunction:: get_fc
+
+.. autofunction:: get_lone_electrons
+"""
+from typing import Optional, NamedTuple
+
+
+_ELEMENT_TO_VALENCE = {
+    'H': 1,
     'C': 4,
     'N': 5,
     'O': 6,
-    'F': 7,
     'P': 5,
     'S': 6,
+
+    # Halogens
+    'F': 7,
     'Cl': 7,
+    'Br': 7,
     'I': 7,
 }
 
-def get_fc(element: str, bonds: int) -> int:
+
+class ElectronInfo(NamedTuple):
+    """Various electron-related properties of an atom."""
+    element: str
+    valence: int
+    bonds: int
+    fc: int
+    lone: int
+    radical: int
+
+
+def calculate_electron_info(element: str, bonds: int, fc: Optional[int] = None) -> int:
+    """
+    Calculates various electron-related properties of an atom.
+
+    :param element: the element of the atom
+    :param bonds: number of bonds attached
+    :param fc:
+        Formal charge of the atom, if known. If not provided, it will be automatically calculated.
+        Our formal charge calculation is optimized for biochemistry simulations, so it might not produce the correct
+        values for sulfur or phosphorous in more complicated molecules.
+    """
+
+    try:
+        valence = _ELEMENT_TO_VALENCE[element]
+    except KeyError:
+        raise ValueError(f"Unsupported element {element}, please manually specify the charge.")
+
+    if fc is None:
+        fc = _get_fc(element, valence, bonds)
+
+    # Note that we calculate lone back from FC for two reasons:
+    # - charge is provided by the user
+    # - we pin the charges of certain element/bond inputs
+    lone = valence - fc - bonds
+
+    # Calculation of radical electrons. This formula appears to work in most cases.
+    if bonds == 1 and lone <= 4: 
+        radical = [0, 1, 0, 3, 2][lone]  # lookup table
+    else:
+        radical = lone % 2
+
+    return ElectronInfo(
+        element=element,
+        valence=valence,
+        bonds=bonds,
+        fc=fc,
+        lone=lone,
+        radical=radical,
+    )
+
+
+def _get_fc(element: str, valence: int, bonds: int) -> int:
     """
     Calculates the formal charge of an atom given its number of bonds.
 
     This is optimized for biochemistry simulations, so it might not produce the correct
-    values for sulfur in more complicated molecules.
+    values for sulfur or phosphorous in more complicated molecules.
+
+    :param element: the element of the atom
+    :param valence: number of valence electrons
+    :param bonds: number of bonds attached to the atom
     """
     if element == 'H':
         return 0
-    if element == 'S':
-        return get_fc('O', bonds)
     if element == 'P':
         if bonds == 5:
             return 0
         if bonds == 6:
-            return 1
-
-    try:
-        valence = ELEMENT_TO_VALENCE[element]
-    except KeyError:
-        raise ValueError(f"Unsupported element {element}, please manually specify the charge.")
+            return -1
 
     unpaired = 8 - valence
 
